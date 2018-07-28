4b496b0cbefe93dae938bbde21734015362ef35c
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-07-28 14:38:48 +0100

    Fix desktop close

--- public/electron.js
+++ public/electron.js
@@ -26,7 +26,7 @@ function createWindow() {
       : `file://${path.join(__dirname, "/../build/index.html")}`
   );
   if (isDev) {
-  mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
   }
 
   mainWindow.once("ready-to-show", () => {
@@ -35,7 +35,7 @@ function createWindow() {
 
   // Prompt users before window close
   mainWindow.on("close", e => {
-    e.preventDefault();
+    // e.preventDefault();
   });
 
   // Emitted when the window is closed.
