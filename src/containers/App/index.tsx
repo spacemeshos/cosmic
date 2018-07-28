e15f2530d3975ffbf2209cfa7ebab93194dda6a8
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-07-28 18:41:34 +0100

    Fix android build issue

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,34 +1,21 @@
 import * as React from "react";
-import Routing from '../../utils/routing';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
 import store from '../../store'
 import SecondPage from '../SecondPage';
 import FirstPage from '../FirstPage';
 
-const { Route, Switch, HashRouter, BrowserRouter } = Routing;
-const userAgent = navigator.userAgent.toLowerCase();
 let App;
-if (userAgent.indexOf(' electron/') > -1 ){
+const history = createMemoryHistory();
   App = () => (
     <Provider store={store}>
-      <HashRouter>
-        <div>
-          <Route exact={true} path="/" component={FirstPage} />
-          <Route exact={true} path="/second" component={SecondPage} />
-        </div>
-      </HashRouter>
-    </Provider>
-  );
-} else {
-  App = () => (
-    <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route exact={true} path="/" component={FirstPage} />
           <Route exact={true} path="/second" component={SecondPage} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
-}
 export default App;
