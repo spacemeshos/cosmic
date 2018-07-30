3f871915563663c3586f0964b09ad5271f6a4dc6
Omidiora Samuel <Samparsky@MACBOOKs-MacBook-Pro.local>
2018-07-30 17:34:28 +0100

    fixed module resolution

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,8 +1,14 @@
+const blacklist = require('metro').createBlacklist;
+
 module.exports = {
   getTransformModulePath() {
     return require.resolve("react-native-typescript-transformer");
   },
   getSourceExts() {
     return ["ts", "tsx"];
+  },
+  getBlacklistRE: function() {
+  console.log(blacklist([/dist\/mac\/.*/]));
+    return blacklist([/dist\/mac\/.*/, "dist/mac/*"]);
   }
 };
