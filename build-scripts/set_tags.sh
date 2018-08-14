acb7b9affc24994b26d69b628078dcfe4126926d
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-08-14 13:46:56 +0100

    Update .travis.yml

---
+++ build-scripts/set_tags.sh
@@ -0,0 +1,24 @@
+BRANCH="master"
+
+# Are we on the right branch?
+if [ "$TRAVIS_BRANCH" = $BRANCH]; then
+
+    # Is this not a Pull Request?
+    if [ "$TRAVIS_PULL_REQUEST" = false]; then
+
+        # Is this not a build which was triggered by setting a new tag?
+        if [-z "$TRAVIS_TAG"]; then
+            echo -e "Starting tag commit. \n"
+
+            git config --global user.email "matrix4u2002@gmail.com"
+            git config --global user.name "iamonuwa"
+
+            # Add tag and push to master.
+            git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+            git push origin --TRAVIS_TAG
+            git fetch origin
+
+            echo -e "Done magic with tags.\n"
+        fi
+    fi
+fi
